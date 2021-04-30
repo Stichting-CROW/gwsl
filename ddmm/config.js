376cf4b0b37ea69bfdf840d7334b99a43a9fcd8c
Redmer Kronemeijer <redmer.kronemeijer@cgi.com>
2021-04-30 11:10:08 +0200

    Update naam

---
+++ ddmm/config.js
@@ -0,0 +1,14 @@
+var respecConfig = { // => https://github.com/stichting-crow/respec/wiki
+    specType: "CROW-DOC",
+    specStatus: "",
+    pubDomain: "gwsl",
+    shortName: "ddmm",
+    // publishDate: "2021-01-01",
+    editors: [
+        {
+            name: "Redmer Kronemeijer",
+            company: "CROW",
+        }
+    ],
+    github: "https://github.com/Stichting-CROW/gwsl",
+};
